5e46cdf40e37f3bf6236905e2f10fdffe830c3b8
Ryosuke Hiramatsu <rhiramat@yahoo-corp.jp>
2014-11-11 19:18:28 +0900

    Initial Commit

---
+++ PhotoFrameworkSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoFrameworkSample
+//
+//  Created by 平松　亮介 on 2014/11/11.
+//  Copyright (c) 2014年 mashroom.in. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
